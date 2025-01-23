d791595e1e99423062237ec6a2e339385d59d613
Tri Ngoc <caotringoc11@gmail.com>
2025-01-23 15:39:47 +0700

    filter by status

--- API/v1/routes/task.route.js
+++ API/v1/routes/task.route.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const Task = require('../../../models/task.model');
-
+// api/v1/tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find({
+    console.log(req.query);
+    const objectFind = {
         deleted: false
-    });
+    };
+    if (req.query.status) {
+        objectFind.status = req.query.status;
+    }
+
+    const tasks = await Task.find(objectFind);
     
     res.json(tasks);
 });
-
+// /api/v1/tasks/detail/:id
 router.get('/detail/:id', async (req, res) => {
     try {
         const id = req.params.id;
