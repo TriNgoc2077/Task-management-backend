f33f3a84fedbac9341b697ca9d9628a2c9623181
Tri Ngoc <caotringoc11@gmail.com>
2025-01-23 15:25:48 +0700

    config route/api

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 const express = require('express');
 const database = require('./config/database');
 require('dotenv').config();
+const routerApiVer1 = require("./API/v1/routes/index.route");
 const app = express();
 const port = process.env.PORT;
 
 database.connect();
-
-const Task = require('./models/task.model');
-
-app.get('/tasks', async (req, res) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
-    
-    res.json(tasks);
-});
-
-app.get('/tasks/detail/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const task = await Task.findOne({
-            _id: id,
-            deleted: false,
-        });
-        res.json(task);
-    } catch(error) {
-        res.json("Not found !");
-    }
-});
+// Router v1 
+routerApiVer1(app);
 
 app.listen(port, () => {
     console.log(`App listening in port ${port}`);
