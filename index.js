ec98226c6d9501c413e0b634a1244013acf9a80a
Tri Ngoc <caotringoc11@gmail.com>
2025-01-23 22:13:02 +0700

    fix CORS error

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const database = require('./config/database');
 const bodyParser = require('body-parser');
+const cors = require('cors'); //Cross-Origin Resource Sharing
 require('dotenv').config();
 const routerApiVer1 = require('./API/v1/routes/index.route');
 const app = express();
 const port = process.env.PORT;
 
 database.connect();
+app.use(cors());
 app.use(bodyParser.json());
 // Router v1 
 routerApiVer1(app);
